ee71e9475397c121c60b1d02b8c5adcae62d4b7e
psy888 <lpsyl888l@yandex.ru>
2019-02-11 15:07:25 +0200

    init

---
+++ app/src/main/java/com/psy888/homework_16_02_2019/MainActivity.java
@@ -0,0 +1,13 @@
+package com.psy888.homework_16_02_2019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
